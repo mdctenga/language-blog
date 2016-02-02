1883351b484fed7505ebd4a3a42032e41e7b0a39
mdctenga <mdctenga@gmail.com>
2016-02-02 09:31:39 -1000

    Add express route stubs

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+// app.METHOD(PATH, HANDLER)
+
 app.get('/', function (req, res) {
   res.send('Hello World');
+});
+
+app.put('/', function (req, res) {
+  res.send('Got a PUT request');
+});
+
+app.post('/user', function (req, res) {
+  res.send('Got a POST request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
 });
 
 app.listen(3000, function () {
